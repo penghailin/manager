67999167d899d335aa51ea8c7ec1a31add17c049
大爷 <101hongling@qq.com>
2019-07-26 16:56:10 +0800

    登录页面布局

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import App from './App.vue'
 Vue.config.productionTip = false
 //导入基础样式
 import './assets/base.less'
+//导入饿了吗ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI)
 //导入路由
 import VueRouter from 'vue-router'
 Vue.use(VueRouter)
