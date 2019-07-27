482353c60651950ef359af5255217489e22ef12a
大爷 <101hongling@qq.com>
2019-07-27 15:57:52 +0800

    完成首页头部布局

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,32 +1,44 @@
 <template>
-    <el-container class="my-box">
-  <el-header class="my-top">
-      
-  </el-header>
-  <el-container>
-    <el-aside class="my-left" width="200px"></el-aside>
-    <el-main class="my-right"></el-main>
+  <el-container class="my-box">
+    <el-header class="my-top">
+      <img src="../assets/logo.png" alt />
+      <h2>电商后台管理平台</h2>
+      <a class="logout" href="#">退出</a>
+    </el-header>
+    <el-container>
+      <el-aside class="my-left" width="200px"></el-aside>
+      <el-main class="my-right"></el-main>
+    </el-container>
   </el-container>
-</el-container>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style lang="less">
-  .my-box{
-    height: 100%;
-    .my-top{
-        background-color: #b3c0d1;
-    }
-    .my-right{
-        background-color:#e9eef3;
-    }
-    .my-left{
+.my-box {
+  height: 100%;
 
+  .my-top {
+    height: 60px;
+    background-color: #b3c0d1;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    h2{
+      color: #fff;
+      
+    }
+    .logout{
+      color: orange;
     }
   }
+  .my-right {
+    background-color: #e9eef3;
+  }
+  .my-left {
+  }
+}
 </style>
